79174fbd934059bc70d89183cfe242bf15547af9
Alireza <a.sarabchi@gmail.com>
2017-01-22 17:14:05 +0330

    naked underscore theme.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package alireza
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'alireza' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'alireza' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'alireza' ), 'alireza', '<a href="https://automattic.com/" rel="designer">Alireza Sarabchi</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
