4ede71899a26a0c19c0fabada5444415fbbeca08
Alireza Sarabchi <a.sarabchi@gmail.com>
2017-02-28 13:15:09 +0330

    Gulp installed.

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+'use strict'
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	minify = require('gulp-clean-css'),
+	concat = require('gulp-concat');
